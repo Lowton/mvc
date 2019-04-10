6cecae70d7d61d642b2d5a4319886500671692bc
akuz1218 <aleksei.kuzmenko@netcracker.com>
2019-04-10 10:25:51 +0400

    update

--- src/main/java/com/mvc/learning/controller/OrderController.java
+++ src/main/java/com/mvc/learning/controller/OrderController.java
@@ -1,4 +1,15 @@
 package com.mvc.learning.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class OrderController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getOrderPage(Model model) {
+        return "order";
+    }
 }
